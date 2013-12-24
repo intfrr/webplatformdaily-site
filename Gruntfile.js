9a2353a851413d639395f7a500dadb5c0d0cf06d
Sime Vidas <sime.vidas@gmail.com>
2013-12-24 19:10:24 +0100

    Added grunt-load-tasks :)

--- Gruntfile.js
+++ Gruntfile.js
@@ -175,18 +175,7 @@ module.exports = function (grunt) {
 		}
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-sass');
-	grunt.loadNpmTasks('grunt-contrib-csslint');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-markdown');
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-autoprefixer');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-html-validation');
+	require('load-grunt-tasks')(grunt);
 
 	grunt.registerTask('css', ['sass', 'csslint', 'autoprefixer', 'cssmin', 'concat:css']);
 	grunt.registerTask('js', ['jshint', 'uglify']);
